c5139c1d48fb1fe3394b9646b11fd25b1bc8bd9b
Erno Hentonen <erno.hentonen@opiframe.com>
2024-01-15 09:34:22 +0200

    shopping app 1

---
+++ react/50_shopping_app/01_stage/backend/routes/shoppingroute.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const itemModel = require("../models/item");
+
+let router = express.Router();
+
+router.get("/shopping",function(req,res) {
+	itemModel.find().then(function(items) {
+		return res.status(200).json(items)
+	}).catch(function(err) {
+		console.log(err);
+		return res.status(500).json({"Message":"Internal server error"});
+	})
+})
+
+router.post("/shopping",function(req,res) {
+	if(!req.body) {
+		return res.status(400).json({"Message":"Bad request"})
+	}
+	if(!req.body.type) {
+		return res.status(400).json({"Message":"Bad request"})
+	}
+	let item = new itemModel({
+		type:req.body.type,
+		count:req.body.count,
+		price:req.body.price
+	})
+	item.save().then(function(item) {
+		return res.status(201).json(item)
+	}).catch(function(err) {
+		console.log(err);
+		return res.status(500).json({"Message":"Internal server error"});
+	})
+})
+
+module.exports = router;
---
+++ react/50_shopping_app/01_stage/backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const shoppingRoute = require("./routes/shoppingroute");
+
+let app = express();
+
+const mongo_url = process.env.MONGODB_URL;
+const mongo_user = process.env.MONGODB_USER;
+const mongo_password = process.env.MONGODB_PASSWORD;
+
+const url = "mongodb+srv://"+mongo_user+":"+mongo_password+"@"+mongo_url+"/shoppingdatabase?retryWrites=true&w=majority"
+
+mongoose.connect(url).then(
+	() => console.log("Connected to MongoDB"),
+	(err) => console.log("Failed to connect to MongoDB. Reason",err)
+)
+
+app.use("/api",shoppingRoute);
+
+let port = process.env.PORT || 3000;
+
+app.listen(port);
+
+console.log("Running in port",port);
+
