bdcaf1255c5e3549b87ff56ef4eedc353cec200c
Erno Hentonen <erno.hentonen@opiframe.com>
2024-01-17 13:31:16 +0200

    shopping app 3

--- react/50_shopping_app/03_stage_redux/frontend/src/main.tsx
+++ react/50_shopping_app/03_stage_redux/frontend/src/main.tsx
@@ -2,11 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import {BrowserRouter} from 'react-router-dom';
+import loginReducer from './reducers/loginReducer';
+import shoppingReducer from './reducers/shoppingReducer';
+import {createStore,Store,AnyAction,applyMiddleware,combineReducers} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+import {AppState} from './types/states';
+
+const rootReducer = combineReducers<AppState>({
+	login:loginReducer,
+	shopping:shoppingReducer
+})
+
+const store:Store<AppState,AnyAction> = createStore(rootReducer,applyMiddleware(thunk));
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
   <BrowserRouter>
+  <Provider store={store}>
     <App />
+  </Provider>
   </BrowserRouter>
   </React.StrictMode>,
 )
