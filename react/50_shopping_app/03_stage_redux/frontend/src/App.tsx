7312077dde98871e3da55ce3f8230345df66035e
Erno Hentonen <erno.hentonen@opiframe.com>
2024-01-22 10:52:45 +0200

    redux 2

--- react/50_shopping_app/03_stage_redux/frontend/src/App.tsx
+++ react/50_shopping_app/03_stage_redux/frontend/src/App.tsx
@@ -5,23 +5,38 @@ import ShoppingList from './components/ShoppingList';
 import Navbar from './components/Navbar';
 import LoginPage from './components/LoginPage';
 import {Routes,Route,Navigate} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 
 function App() {
 
 	const action = useAction();
 	
+	const selector = (state) => {
+		let error = state.shopping.error
+		if(state.login.error) {
+			error = state.login.error
+		}
+		return {
+			isLogged:state.login.isLogged,
+			loading:state.login.loading,
+			error:error
+		}
+	}
+	
+	const state = useSelector(selector);
+	
 	let messageArea = <h4 style={{"height":50,"textAlign":"center"}}></h4>
 	
-	if(action.state.loading) {
+	if(state.loading) {
 		messageArea = <h4 style={{"height":50,"textAlign":"center"}}>Loading ...</h4>
 	}
-	if(action.state.error) {
+	if(state.error) {
 		messageArea = <h4 style={{"height":50,"textAlign":"center"}}>{action.state.error}</h4>
 	}
-	if(action.state.isLogged) {
+	if(state.isLogged) {
 	return (
 		<>
-			<Navbar logout={action.logout} isLogged={action.state.isLogged} user={action.state.user}/>
+			<Navbar/>
 				{messageArea}
 			<Routes>
 				<Route path="/" element={<ShoppingList list={action.state.list} remove={action.remove} edit={action.edit}/>}/>
@@ -33,7 +48,7 @@ function App() {
 	}else {
 		return(
 		<>
-			<Navbar logout={action.logout} isLogged={action.state.isLogged} user={action.state.user}/>
+			<Navbar />
 				{messageArea}
 			<Routes>
 				<Route path="/" element={<LoginPage register={action.register} login={action.login} setError={action.setError}/>}/>
--- react/50_shopping_app/03_stage_redux/frontend/src/components/Navbar.tsx
+++ react/50_shopping_app/03_stage_redux/frontend/src/components/Navbar.tsx
@@ -1,13 +1,22 @@
 import {Link} from 'react-router-dom';
+import {useSelector,useDispatch} from 'react-redux';
+import {logout} from '../actions/loginActions';
+import {ThunkDispatch} from 'redux-thunk';
+import {AnyAction} from 'redux';
 
-interface Props {
-	logout():void;
-	isLogged:boolean;
-	user:string;
-}
-
-const Navbar = (props:Props) => {
-	if(props.isLogged) {
+const Navbar = (props) => {
+	
+	const selector = (state) => {
+		return {
+			isLogged:state.login.isLogged,
+			token:state.login.token,
+			user:state.login.user
+		}
+	}
+	const state = useSelector(selector);
+	const dispatch:ThunkDispatch<any,any,AnyAction> = useDispatch();
+	
+	if(state.isLogged) {
 		return(
 			<nav className="navbar navbar-expand-lg navbar-light bg-light">
 				<p className="navbar-brand" style={{marginLeft:10}}>Shopping App</p>
@@ -19,10 +28,10 @@ const Navbar = (props:Props) => {
 						<Link to="/form" className="nav-link">Add new item</Link>
 					</li>
 					<li className="nav-item" style={{marginLeft:10}}>
-						<p style={{color:"blue"}} className="nav-link">Logged in as {props.user}</p>
+						<p style={{color:"blue"}} className="nav-link">Logged in as {state.user}</p>
 					</li>
 					<li className="nav-item" style={{marginLeft:10}}>
-						<Link to="/" onClick={props.logout} className="nav-link">Logout</Link>
+						<Link to="/" onClick={() => dispatch(logout(state.token))} className="nav-link">Logout</Link>
 					</li>
 				</ul>
 			</nav>
