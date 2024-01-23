3aaf9129716f6d8020aeabb2acf40c5edd0ec49d
Erno Hentonen <erno.hentonen@opiframe.com>
2024-01-23 10:31:50 +0200

    simple context

--- react/12_simple_context/src/App.tsx
+++ react/12_simple_context/src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React,{ useState } from 'react'
+import ThemeContext,{themes,ThemeType} from './context/ThemeContext';
+import Headline from './components/Headline';
+import Paragraph from './components/Paragraph';
+import ThemeButton from './components/ThemeButton';
+
+interface State {
+	theme:ThemeType;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [state, setState] = useState<State>({
+		theme:themes.dark
+	})
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const toggleTheme = () => {
+		if(state.theme === themes.dark) {
+			setState({
+				theme:themes.light
+			})
+		} else {
+			setState({
+				theme:themes.dark
+			})
+		}
+	}
+
+	return (
+		<ThemeContext.Provider value={state.theme}>
+			<div style={{"margin":"auto","textAlign":"center"}}>
+				<Headline>
+				Pääkaupunkiseudun päiväkodit kiinni kahdeksi päiväksi
+				</Headline>
+				<Paragraph>
+				Hoitajajärjestöt Tehy ja Super sekä ammattiliitto JHL ovat päättäneet kahden vuorokauden lakoista varhaiskasvatukseen pääkaupunkiseudulla.
+
+Lakko koskee työvuoroja, jotka alkavat 31.1. kello 6.00 ja 1.2. kello 21 välisenä aikana.
+
+Julkisia ja yksityisiä varhaiskasvatuksen työpaikkoja koskevat lakot koskevat on julistettu Helsingin, Espoon, Kauniaisten ja Vantaan alueille.
+
+Superin tiedotteen mukaan lakko ei koske ympärivuorokautista varhaiskasvatusta, kodin perhepäivähoitoa, ryhmäperhepäivähoitoa, yksityistä perhepäivähoitoa tai hoitoapupalvelua.
+				</Paragraph>
+				<ThemeButton toggleTheme={toggleTheme}/>
+			</div>
+		</ThemeContext.Provider>
+	)
 }
 
 export default App
