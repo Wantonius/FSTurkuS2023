398c97fe872ddf5fef2ed7ff414e6bed06c0a47a
Wantonius <hentonen@gmail.com>
2024-02-29 10:21:47 +0200

    angular esimerkit

---
+++ angular/l-shoppingapp/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { LoginPage } from './components/loginpage.component';
+import { ShoppingList } from './components/shoppinglist.component';
+import {ShoppingForm } from './components/shoppingform.component';
+import {Navbar} from './components/navbar.component';
+
+import { LoginService } from './services/loginservice.service';
+import { ShoppingService} from './services/shoppingservice.service';
+@NgModule({
+  declarations: [
+    AppComponent,
+	LoginPage,
+	ShoppingForm,
+	ShoppingList,
+	Navbar
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+	FormsModule,
+	HttpClientModule
+  ],
+  providers: [LoginService,ShoppingService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
